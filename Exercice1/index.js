b38cae2e03f2148a0ba127920fe66c7f48e819a5
Fred jeune DEV en devenir <fredstessy@outlook.fr>
2025-03-20 11:23:31 +0100

    final commit

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,20 @@
 //votre code ici
+function pairNumbers(min, max) {
+    let result = "";
+  
+    if (min > max) {
+      return result;
+    }
+  
+    for (let i = min; i <= max; i++) {
+      if (i % 2 === 0) {
+        result += i + ",";
+      }
+    }
+  
+    // Supprimer la dernière virgule
+    return result.slice(0, -1);
+  }
+  
+  export default pairNumbers;
 
-export default pairNumbers
